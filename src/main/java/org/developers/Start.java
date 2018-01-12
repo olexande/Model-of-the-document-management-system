e9cdaa3b0df51053c596c6c4d67f21b74c68c972
vm-certo <vm.certo@gmail.com>
2018-01-12 23:42:12 +0700

    Вызов инициализации (обязателен)

--- src/main/java/org/developers/Start.java
+++ src/main/java/org/developers/Start.java
@@ -1,7 +1,12 @@
 package org.developers;
 
+import org.developers.business_logic.Initialization;
+
 public class Start {
-    public static void main( String[] args ){
-        System.out.println( "Check" );
+    public static void main(String[] args) {
+        //todo: определить метоположение основного тома/каталога
+        //todo: обязательный блок
+        String pathToBaseLocation = "M://";
+        Initialization.init(pathToBaseLocation);
     }
 }
