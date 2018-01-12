fc000423d0e948d997bf5eaf260fd5ca35fdd318
vm-certo <vm.certo@gmail.com>
2018-01-13 00:33:37 +0700

    Добавлен интерфйес "Журнал документов". Изменены комментарии

---
+++ src/main/java/org/developers/business_logic/model/JournalOfDocuments.java
@@ -0,0 +1,12 @@
+package org.developers.business_logic.model;
+
+//Миронов - Внедрение  документов и отчётов{
+//Журнал документов
+public interface JournalOfDocuments {
+    //название журнала
+    String getTitle();
+
+    //получение документов
+    Document getDocument();
+}
+//Миронов - Внедрение  документов и отчётов}
