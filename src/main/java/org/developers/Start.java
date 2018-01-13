1f7530df5f0cc0a7437a8c386270e07373bf9db0
vm-certo <vm.certo@gmail.com>
2018-01-13 15:51:06 +0700

    Примечание

--- src/main/java/org/developers/Start.java
+++ src/main/java/org/developers/Start.java
@@ -1,7 +1,5 @@
 package org.developers;
 
-import org.developers.business_logic.Initialization;
-
 public class Start {
     public static void main(String[] args) {
         //todo: определить метоположение основного тома/каталога
