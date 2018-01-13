6fcde236a369f069f5f0ad3446cd3de6e61d0641
vm-certo <vm.certo@gmail.com>
2018-01-13 14:33:53 +0700

    Изменения в соответствии с изменениями

--- src/main/java/org/developers/Start.java
+++ src/main/java/org/developers/Start.java
@@ -1,13 +1,1 @@
-package org.developers;
-
-import org.developers.model.User;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class Start {
-    public static void main(String[] args){
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("all-context.xml");
-        User user = ctx.getBean("user", User.class);
-        System.out.println(user.getName());
-    }
-}
+package org.developers;import org.developers.model.Users.Role;import org.springframework.context.ApplicationContext;import org.springframework.context.support.ClassPathXmlApplicationContext;public class Start {    public static void main(String[] args){        ApplicationContext ctx = new ClassPathXmlApplicationContext("all-context.xml");        Role user = ctx.getBean("admin", Role.class);        System.out.println(user.getName());        user.getRights().forEach((key, value)->System.out.println(key.getAbbr() + " -> " + value));    }}
