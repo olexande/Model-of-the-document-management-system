1402bd1a49569b20565966edb623bf96ac459392
vm-certo <vm.certo@gmail.com>
2018-01-13 19:44:41 +0700

    Инициализация ветки "Документ инфентаризации"

---
+++ src/main/java/org/developers/model/Documents/impl/Inventory.java
@@ -0,0 +1,48 @@
+package org.developers.model.Documents.impl;
+
+import org.developers.model.Documents.Document;
+import org.developers.model.Documents.TypeOfDocument;
+
+import java.time.LocalDate;
+
+public class Inventory implements Document {
+    @Override
+    public TypeOfDocument getType() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getNumber() {
+        return null;
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return null;
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void signature(Object o1, Object o2) {
+
+    }
+}
