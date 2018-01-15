b993f14dc3dd6bd1a2ba875b1cf63d21011e1fc5
vm-certo <vm.certo@gmail.com>
2018-01-15 19:12:46 +0700

    Проверка работоспособности

--- src/main/java/org/developers/Start.java
+++ src/main/java/org/developers/Start.java
@@ -1,10 +1,12 @@
 package org.developers;
 
+import org.developers.system.LogOfSystem;
+import org.developers.system.LogType;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Start {
     public static void main( String[] args ){
-
+        LogOfSystem.writeLog(LogType.EMPTY, "чушь");
     }
 }
