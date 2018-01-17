b0919916a8cbb39553d569af2ab200a1752dab2c
Alexander Macakov <macakovaleksandr@gmail.com>
2018-01-17 16:59:44 +0400

    добавлена реализация отчета кассовой смены

--- src/main/java/org/developers/model/Cashbox.java
+++ src/main/java/org/developers/model/Cashbox.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Cashbox {
 
     private Employee saler;
-    private Map<Product, Integer> list;  // список продуктов
+    private Map<Product, Integer> list = new LinkedHashMap<>();;  // список продуктов
     private int numberCheck = 0;
     private int productCout = 0;
     private double totalSum = 0;
@@ -18,8 +18,9 @@ public class Cashbox {
 
     public Cashbox(Employee saler) {
         this.saler = saler;
-        list = new LinkedHashMap<>();
     }
+
+
 
     private void selling() throws IOException {
 
@@ -47,7 +48,8 @@ public class Cashbox {
 
     }
 
-    public void addProduct(String name, int count) {
+
+    private void addProduct(String name, int count) {
         Product product = new Product();
         if (product.getProductTitle().equals(name)) {
             totalSum += product.getProductPrice();
@@ -58,15 +60,30 @@ public class Cashbox {
             System.out.println("товар не найден");
     }
 
-    public void pay(Check check) {
+    private void pay(Check check) {
 
     }
 
-    public boolean verification(Check check) {
+    private boolean verification(Check check) {
         return true;
     }
 
-    private void diffToBase(){
+    private void closedShifts(Map<Integer, Check> checkList){
+        double rezultSumm = 0;
+        int rezultCount = 0;
+        for (Map.Entry<Integer, Check> check: checkList.entrySet()) {
+            rezultSumm += check.getValue().getTotalSum();
+            rezultCount += check.getKey();
+        }
 
+        if (rezultSumm == totalSum && rezultCount == productCout) {
+            // создается лог
+            checkList.clear();
+            list.clear();
+            numberCheck = 0;
+            productCout = 0;
+        }
+        else
+            System.out.println("Суммы не сходятся, есть проблемные чеки");
     }
 }
