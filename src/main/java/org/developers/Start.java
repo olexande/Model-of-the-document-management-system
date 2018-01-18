0d1a640fc3515ceb8153fd687993663fd361301e
vm-certo <vm.certo@gmail.com>
2018-01-18 17:59:22 +0700

    Убрал мусор

--- src/main/java/org/developers/Start.java
+++ src/main/java/org/developers/Start.java
@@ -1,21 +1,7 @@
 package org.developers;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.sql.SQLException;
-
 public class Start {
-    public static void main( String[] args ) throws SQLException {
-        Organization.setNameSeller("Торговля");
-        Organization.setInn(111111111);
-        Organization.setSystemTaxation("orm");
-
-
-        Check check1 = new Check(1232414);
-        System.out.println(check1.toString());
-
-
+    public static void main( String[] args ) {
 
     }
 }
