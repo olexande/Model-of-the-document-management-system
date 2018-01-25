e5946cf255b56d02d5831a929fb6cb810b347df6
Alexander Macakov <macakovaleksandr@gmail.com>
2018-01-25 12:25:15 +0400

    коммит для слияния веток, не несет полезной информации

--- src/main/java/org/developers/Start.java
+++ src/main/java/org/developers/Start.java
@@ -1,7 +1,10 @@
 package org.developers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Start {
     public static void main( String[] args ){
-
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("products.xml");
     }
 }
